74626c362f2926401c5d0dadd1c88f6e92e97d5a
MyDreamComesTrue <aziaboukoami@gmail.com>
2024-05-16 23:41:19 +0000

    Add Python3 keyword program

---
+++ day_2/my_keyword.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Author: HealthTechKid
+Date: 16 / 05 / 2024
+Purpose: A program to print Python keywords and the total number of keywords
+"""
+
+import keyword
+
+# Print the list of Python keywords
+print(keyword.kwlist)
+
+# Print the total number of Python keywords
+print(len(keyword.kwlist))
