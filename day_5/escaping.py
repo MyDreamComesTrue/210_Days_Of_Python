08af92bb8b04e273f287c2229e561bf60cc5ce1e
MyDreamComesTrue <aziaboukoami@gmail.com>
2024-05-19 17:00:25 +0000

    Add escaping file

---
+++ day_5/escaping.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+Author: HealthTechKid
+Date: 19 / 05 / 2024
+Purpose: Script to escape special characters
+"""
+
+print("My mom name is \"Diamond\", and she's a star(*).")
