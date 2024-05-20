b2b1d8b0ce0c35c1caeaa2c436a26346e8b8719b
MyDreamComesTrue <aziaboukoami@gmail.com>
2024-05-20 22:23:24 +0000

    Add the partition program

---
+++ day_6/str_partition.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Author: HealthTechKid
+Date: 20 / 05 / 2024
+Purpose: A program that partitions a string
+"""
+
+my_string = "Here is an example of a string"
+separator = "of"
+result = my_string.rpartition(separator)
+print(result)
