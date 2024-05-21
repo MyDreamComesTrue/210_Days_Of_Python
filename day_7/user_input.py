45a129080d21d5d6555870c2203627a70c5fbdec
MyDreamComesTrue <aziaboukoami@gmail.com>
2024-05-21 23:06:43 +0000

    Add user_input file

---
+++ day_7/user_input.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Author: HealthTechKid
+Date: 21 / 05 / 2024
+Purpose: A program to print Python keywords and the total number of keywords
+"""
+
+x = int(input("What's your age: "))
+y = input("What's your name?: ")
+print("My name is", y, "and i'm", x, "years old.")
